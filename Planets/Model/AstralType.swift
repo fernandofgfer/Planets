71ca022f67fbd7aa5f458efdfac1fdd9ab275c2f
Fernando Garcia Fernandez <fernandogarciafernandez@MacBook-Pro-de-Fernando.local>
2024-11-11 16:16:30 +0100

    Added model for first stage

---
+++ Planets/Model/AstralType.swift
@@ -0,0 +1,11 @@
+//
+//  AstralType.swift
+//  Planets
+//
+//  Created by Fernando Garcia Fernandez on 11/11/24.
+//
+
+enum AstralType: String, Decodable {
+    case space = "SPACE"
+    case Polyanet = "POLYANET"
+}
---
+++ Planets/Model/Map.swift
@@ -0,0 +1,10 @@
+//
+//  Map.swift
+//  Planets
+//
+//  Created by Fernando Garcia Fernandez on 11/11/24.
+//
+
+struct Map: Decodable {
+    let goal: [[AstralType]]
+}
