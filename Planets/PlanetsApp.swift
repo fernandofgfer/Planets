18fcd3b7619f66703c97bc226036ee449fb5e017
Fernando Garcia Fernandez <fernandogarciafernandez@MacBook-Pro-de-Fernando.local>
2024-11-11 16:10:38 +0100

    First commit to add the project

---
+++ Planets/PlanetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetsApp.swift
+//  Planets
+//
+//  Created by Fernando Garcia Fernandez on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct PlanetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlanetsTests/PlanetsTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlanetsTests.swift
+//  PlanetsTests
+//
+//  Created by Fernando Garcia Fernandez on 11/11/24.
+//
+
+import Testing
+
+struct PlanetsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
